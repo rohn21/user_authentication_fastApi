83d0ef8347a3e01ace4b2dfe304a112aeb8c11c2
Rohan_21 <rohn2100@gmail.com>
2024-09-02 23:50:59 +0530

    update method

--- user_auth/utils/pwd_hash.py
+++ user_auth/utils/pwd_hash.py
@@ -1,9 +1,17 @@
+"""
+    This file contains following functionality
+    1. Convert user entered password into hashed password that will to increase password security
+    2. Verifies the actual defined/choosen password by user with stored hashed password
+        - will check both password and return the appropriate result in Boolean data_type
+"""
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# function to convert a password into hashed password
 def pass_hash(password:str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(default_password: str, hashed_password: str) -> bool:
+# function to verify both default_password and hashed password are same or not
+def verify_password(default_password: str, hashed_password: str) -> bool:   # return in boolean data_type
     return pwd_context.verify(default_password, hashed_password)
